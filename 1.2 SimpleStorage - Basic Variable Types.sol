d8a96a6a605713ab3d3a9f2d29e701c8236686df
wahidonchain <cyberwahid01@outlook.com>
2025-10-04 13:35:16 +0100

    Narrow To Favourite Number and Explaining = 0 In Comments

--- 1.2 SimpleStorage - Basic Variable Types.sol
+++ 1.2 SimpleStorage - Basic Variable Types.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.18; // solidity versions I am using for this purpose (and a
 
 contract SimpleStorage {
     // Basic Solidity Types: boolean, uint, int, address, bytes
-    uint256 favouriteNumber; // defaults to 0 if no value specified, same as typing uint256 favouriteNumber = 0;
+    uint256 favouriteNumber; // defaults to 0 if no value specified, same as typing uint256 favouriteNumber = 0; 
     
 }
