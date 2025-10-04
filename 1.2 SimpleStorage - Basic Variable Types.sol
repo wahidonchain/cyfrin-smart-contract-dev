a41eec158a41811392e841c0f93feea2c307882a
wahidonchain <cyberwahid01@outlook.com>
2025-10-04 13:21:28 +0100

    C-Solidity 1.2 - Narrow to Favourite Number

---
+++ 1.2 SimpleStorage - Basic Variable Types.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18; // solidity versions I am using for this purpose (and anything greater than this)
+
+contract SimpleStorage {
+    // Basic Solidity Types: boolean, uint, int, address, bytes
+    uint256 favouriteNumber; // defaults to 0 if no value specified
+    
+}
